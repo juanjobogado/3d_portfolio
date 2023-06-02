baf4fcb46ff8c0491e06fd7d42de0a690cf1b028
Juan Bogado <juanjobogado13@gmail.com>
2023-06-02 11:55:18 -0300

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import {jblogo, menu, close } from "../assets";
 const Navbar = () => {
 
   const [active, setActive] = useState("")
+  const [toggle, setToggle] = useState(false);
 
   return (
     <nav
@@ -34,8 +35,42 @@ const Navbar = () => {
         <a href={`#${link.id}`}>{link.title}</a>
       </li>
     ))}
-
   </ul>
+
+  <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+              {navLinks.map((nav) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === nav.title ? "text-white" : "text-secondary"
+                  }`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(nav.title);
+                  }}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+   
+
 </div>
     </nav>
   )
