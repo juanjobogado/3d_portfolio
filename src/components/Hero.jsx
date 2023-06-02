73a200bcc8b60e27ffd14395b76c56017307d30f
Juan Bogado <juanjobogado13@gmail.com>
2023-06-02 12:21:09 -0300

    p with my description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,6 +14,9 @@ const Hero = () => {
 
       <div>
         <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Juan José</span></h1>
+        <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          I develop full stack mobile and web applications.
+        </p>
       </div>
 
       </div>
