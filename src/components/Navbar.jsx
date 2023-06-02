44f5fdceeed1cb22c02c9d88c0e7f05d523ba00d
Juan Bogado <juanjobogado13@gmail.com>
2023-06-02 12:01:56 -0300

    navbar ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
     }}
   >
     <img src={jblogo} alt="logo" className='w-9 h-9 object-contain'/>
-    <p className='text-white text-[18px] font-bold cursor-pointer'>Juan José <span className='sm:block hidden'>| Full Stack Developer</span></p>
+    <p className='text-white text-[18px] font-bold cursor-pointer flex'>Juan José &nbsp; <span className='sm:block hidden'>| Full Stack Developer</span></p>
   </Link>
 
   <ul className='list-none hidden sm:flex flex-row gap-10'>
